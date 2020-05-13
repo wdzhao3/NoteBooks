7ee07324fb2caae11a2bc701567ee64d858e0571
weidongzhao3 <39546715+weidongzhao3@users.noreply.github.com>
2020-05-13 11:16:43 +0800

    基于tcp的聊天小程序

--- demo/tcp/index.js
+++ demo/tcp/index.js
@@ -1,12 +1,64 @@
 const net = require('net')
-
-let count = 0
+// telnet 127.0.0.1 3001
+let count = 0,
+    users = {}
 const server = net.createServer((conn) => {
-    conn.write('\n135154'
-    + '\n' + count
-    + '\n4646464'
-    + '\n > 44646464 enter: ')
-    count++
+    // 设置编码
+    conn.setEncoding('utf8')
+
+    
+    let nickname;
+    let res = '';
+    // 客户端发来的数据
+    conn.on('data', (data) => {
+        // data = data.replace('\r\n', '') // 清楚回车键
+        console.log(data)
+        if(data != '\r\n') {
+            res += data
+            return
+        } else {
+            data = res
+        }
+        if (!nickname) {
+            if (users[data]) {
+                conn.write('name already in use. try again')
+                return
+            } else {
+                count++
+                nickname = data
+                users[nickname] = conn
+
+                broadcast('\r\nwelcome ' + nickname + ' joined the Room \r\n')
+            }
+        } else {
+            // 其他链接的客户端视为聊天信息
+            broadcast(' > ' + nickname + ': ' + data + '\r\n', true)
+        }
+        res = ''
+    })
+    // 客户端显示的数据
+    conn.write('\r\n now Room(' + count + ')'
+        + '\r\n Login'
+        + '\r\n > your name and press enter: ')
+    // 客户端连接关闭事件,错误关闭时也会触发close，而不会触发end事件
+    conn.on('close', () => {
+        console.log('链接-1')
+        delete users[nickname]
+        count--
+        broadcast(' > ' + nickname + ' : left the Room' +  + '\r\n')
+    })
+    // 连接发生错误时
+    conn.on('error', () => {
+        console.log('这里连接发生了错误！')
+    })
+    function broadcast(msg, exceptMyself) {
+        for (let i in users) {
+            if (!exceptMyself || i != nickname) {
+                users[i].write(msg)
+            }
+        }
+    }
+
     console.log('\033[90m  new connection!\033[90m')
 })
 
