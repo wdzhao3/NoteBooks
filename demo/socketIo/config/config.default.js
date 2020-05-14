8a6f0a168118b4e751cce37ba110d068a120d487
weidongzhao3 <39546715+weidongzhao3@users.noreply.github.com>
2020-05-14 10:23:21 +0800

    添加Proxy和Reflect，添加socketIo的demo

---
+++ demo/socketIo/config/config.default.js
@@ -0,0 +1,44 @@
+/* eslint valid-jsdoc: "off" */
+
+'use strict';
+
+/**
+ * @param {Egg.EggAppInfo} appInfo app info
+ */
+module.exports = appInfo => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  const config = exports = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1589360995927_3480';
+
+  // add your middleware config here
+  config.middleware = [];
+
+  config.io = {
+    init: {},
+    namespace: {
+      '/': {
+        connectionMiddleware: [],
+        packetMiddleware: []
+      },
+      '/example': {
+        connectionMiddleware: [],
+        packetMiddleware: []
+      }
+    }
+  }
+
+  // add your user config here
+  const userConfig = {
+    // myAppName: 'egg',
+  };
+
+  return {
+    ...config,
+    ...userConfig,
+  };
+};
